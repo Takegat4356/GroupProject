d54edf521062a9f9802d9499e0dc3c87847b5b36
Takegat4356 <ssrankin@umich.edu>
2014-07-31 16:27:53 -0400

    Initial Commit
    Initial Commit

---
+++ Book/WebApplication1/Controllers/RecipeController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class RecipeController : Controller
+    {
+        private BookofRecipesEntities db = new BookofRecipesEntities();
+
+        // GET: Recipe
+        public ActionResult Index(string sortOrder, string searchString)
+        {
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "Name" : "";
+            ViewBag.NationalitySortParm = sortOrder == "Nationality" ? "Nationality" : "Nationality";
+            var Recipe = from r in db.Recipes
+                         select r;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                Recipe = Recipe.Where(r => r.Name.ToUpper().Contains(searchString.ToUpper()));
+            }
+            switch (sortOrder)
+            {
+                case "Nationality":
+                    Recipe = Recipe.OrderByDescending(r => r.Nationality);
+                    break;
+                case "Name":
+                    Recipe = Recipe.OrderByDescending(r => r.Name);
+                    break;
+                default:
+                    Recipe = Recipe.OrderBy(r => r.Name);
+                    break;
+            }
+            return View(Recipe.ToList());
+        }
+        //public ActionResult Index()
+        //{
+        //    return View(db.Recipes.ToList());
+        //}
+
+        //// GET: Recipe/Details/5
+        //public ActionResult Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Recipe recipe = db.Recipes.Find(id);
+        //    if (recipe == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(recipe);
+        //}
+
+        // GET: Recipe/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Recipe/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Nationality,PrepTime,CookTime,NbrOfPeople,NbrOfSteps,Ingredients")] Recipe recipe)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Recipes.Add(recipe);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(recipe);
+        }
+
+        // GET: Recipe/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Recipe recipe = db.Recipes.Find(id);
+            if (recipe == null)
+            {
+                return HttpNotFound();
+            }
+            return View(recipe);
+        }
+
+        // POST: Recipe/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Nationality,PrepTime,CookTime,NbrOfPeople,NbrOfSteps,Ingredients")] Recipe recipe)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(recipe).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(recipe);
+        }
+
+        // GET: Recipe/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Recipe recipe = db.Recipes.Find(id);
+            if (recipe == null)
+            {
+                return HttpNotFound();
+            }
+            return View(recipe);
+        }
+
+        // POST: Recipe/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Recipe recipe = db.Recipes.Find(id);
+            db.Recipes.Remove(recipe);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
